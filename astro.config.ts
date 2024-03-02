440e6816162fef227fad1071035a328e11c29377
Leon Dore <leon.dore@gmail.com>
2024-03-02 19:43:39 -0400

    Complete remaining inner pages; add vercel adapter and turn on hybrid rendering

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
+import tailwind from '@astrojs/tailwind';
 import alpinejs from '@astrojs/alpinejs';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs({ entrypoint: '/src/alpine' })],
+  output: 'hybrid',
+  integrations: [
+    tailwind(),
+    alpinejs({
+      entrypoint: '/src/alpine',
+    }),
+  ],
+  adapter: vercel(),
 });
