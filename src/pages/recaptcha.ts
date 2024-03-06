dad0b194a23596e8409790b293b58145242501a3
Leon Dore <leon.dore@gmail.com>
2024-03-05 20:35:18 -0400

    error handling for recaptcha endpoint

--- src/pages/recaptcha.ts
+++ src/pages/recaptcha.ts
@@ -13,18 +13,18 @@ const newResponse = (body: Response | ErrorResponse, status: number) =>
   new Response(JSON.stringify(body), { status });
 
 export async function POST({ request }: { request: Request }) {
-  const data = await request.json();
-
-  const recaptchaURL = 'https://www.google.com/recaptcha/api/siteverify';
-  const requestHeaders = {
-    'Content-Type': 'application/x-www-form-urlencoded',
-  };
-  const requestBody = new URLSearchParams({
-    secret: import.meta.env.RECAPTCHA_SECRET_KEY,
-    response: data.recaptcha,
-  });
-
   try {
+    const data = await request.json();
+
+    const recaptchaURL = 'https://www.google.com/recaptcha/api/siteverify';
+    const requestHeaders = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    };
+    const requestBody = new URLSearchParams({
+      secret: import.meta.env.RECAPTCHA_SECRET_KEY,
+      response: data.recaptcha,
+    });
+
     const response = await fetch(recaptchaURL, {
       method: 'POST',
       headers: requestHeaders,
